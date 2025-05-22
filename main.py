0b78281701c02c0637368f5d9d8466057ab4336e
RussNavas <navasr@oregonstate.edu>
2025-05-22 12:07:01 -0500

    created circleshape module creates base class for game objects will have draw and update methods.

--- main.py
+++ main.py
@@ -8,16 +8,31 @@ from constants import (SCREEN_WIDTH,
 
 
 def main():
+    # initialize pygame
     pygame.init()
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+
+    # set the screen dimensions
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # create Clock object
+    fps = pygame.time.Clock()
+    # init variable for delta time
+    dt = 0
+
+    # Begin game loop
     while True:
+        # iterate through events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+            # set and capture the frame rate each iteration
+            dt = fps.tick(60)/1000
+
+        # set the background to black
         screen.fill("black")
         pygame.display.flip()
 
